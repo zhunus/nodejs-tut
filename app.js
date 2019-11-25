259a473e45fc328a034d0f64264b56713fbe6401
Zhunis Gaziz <zhunis_gaziz@epam.com>
2019-11-25 17:53:19 +0600

    WIP html resolving

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(8080);
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
+const router = express.Router();
+
+router.get('/add-product', (req, res) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+router.post('/add-product', (req, res, next) => {
+  res.redirect('/');
+});
+
+module.exports = router;
